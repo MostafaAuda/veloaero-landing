0611bd28d46dbe2c4bbf608f1e799069a5b790a3
Mostafa Auda <mostafa.auda@outlook.com>
2021-10-30 23:29:11 +0200

    Delete site directory

--- site/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.header__reigster').on('click', function () {
-    $(this).toggleClass('animate__animated');
-    $('.header__register_form').fadeToggle();
-  });
-
-  AOS.init();
-});
